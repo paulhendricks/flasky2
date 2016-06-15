b998b3198dc587ce7e6107b79509b193b00b901c
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-06-15 13:34:03 -0400

    added license and other flask extensions

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_script import Manager
+from flask_moment import Moment
 
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
